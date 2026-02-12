e64eaf55e307c192a2c8b435720d05447e2d9f58
Anuraggod <kalpanasinghchauhan20@gmail.com>
2026-02-12 10:00:56 +0530

    feat(init): initial project setup

--- src/project1/Loop.java
+++ src/project1/Loop.java
@@ -2,7 +2,7 @@ package project1;
 
 public class Loop {
     public static void main (String [] agrs) {
-        System.out.print("hello eshita");
+        System.out.print("hello");
     }
     }
 
